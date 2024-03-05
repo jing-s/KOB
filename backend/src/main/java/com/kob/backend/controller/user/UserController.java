455a8f65dfd263a537a09bd84022c586218a5dc1
jing-s <jlx1999s@gmail.com>
2024-03-05 13:15:00 +0800

    add encode password

--- backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ public class UserController {
     @Autowired
     UserMapper userMapper;
 
-    @GetMapping("/user/all")
+    @GetMapping("/user/all/")
     public List<User> getAll() {
         return userMapper.selectList(null);
     }
@@ -33,7 +35,9 @@ public class UserController {
             @PathVariable int userId,
             @PathVariable String username,
             @PathVariable String password) {
-        User user = new User(userId, username, password);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodePassword = passwordEncoder.encode(password);
+        User user = new User(userId, username, encodePassword);
         userMapper.insert(user);
         return "Add User Successfully";
     }
