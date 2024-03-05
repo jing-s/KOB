9106b55d31b7767f9e3cf604ceb43b3eb6be10ff
jing-s <jlx1999s@gmail.com>
2024-03-06 07:45:26 +0800

    debug about getinfo

--- web/src/App.vue
+++ web/src/App.vue
@@ -30,6 +30,20 @@ export default {
         console.log(resp);
       }
     })
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3ZDNhMDY3ZjI5NmI0ZmQ4OTlhNWYyMmVjMmVlNTUyOSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTcwOTY4MjA5MiwiZXhwIjoxNzEwODkxNjkyfQ.I9mYWvaRf2xIhABQ8Pg6qRxFwXIO-2z_vWTrjWqWP14",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
   },
 }
 </script>
