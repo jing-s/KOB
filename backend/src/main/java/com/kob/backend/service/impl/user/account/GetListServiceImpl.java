fab26ad664e54b603a7492cdb9b28d534c747f49
jing-s <jlx1999s@gmail.com>
2024-03-07 14:49:57 +0800

    complete userpage Controller

--- backend/src/main/java/com/kob/backend/service/impl/user/account/GetListServiceImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/GetListServiceImpl.java
@@ -1,47 +1,33 @@
-package com.kob.backend.service.impl.user.bot;
+package com.kob.backend.service.impl.user.account;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kob.backend.mapper.BotMapper;
 import com.kob.backend.pojo.Bot;
 import com.kob.backend.pojo.User;
 import com.kob.backend.service.impl.UserDetailsImpl;
-import com.kob.backend.service.user.bot.RemoveService;
+import com.kob.backend.service.user.bot.GetListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
-public class RemoveServiceImpl implements RemoveService {
+public class GetListServiceImpl implements GetListService {
     @Autowired
     private BotMapper botMapper;
     @Override
-    public Map<String, String> remove(Map<String, String> data) {
+    public List<Bot> getList() {
         UsernamePasswordAuthenticationToken authenticationToken =
                 (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
         UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
         User user = loginUser.getUser();
 
-        int bot_id = Integer.parseInt(data.get("bot_id"));
-        Bot bot = botMapper.selectById(bot_id);
+        QueryWrapper<Bot> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", user.getId());
 
-        Map<String, String> map = new HashMap<>();
-
-        if (bot == null) {
-            map.put("error_message", "Bot不存在或已被删除");
-            return map;
-        }
-
-        if (!bot.getUserId().equals(user.getId())) {
-            map.put("error_message", "没有权限删除Bot");
-            return map;
-        }
-
-        botMapper.deleteById(bot_id);
-
-        map.put("error_message", "success");
-        return map;
+        return botMapper.selectList(queryWrapper);
     }
 }
--- web/src/views/user/bot/UserBotindexView.vue
+++ web/src/views/user/bot/UserBotindexView.vue
@@ -17,26 +17,80 @@ export default {
         // const store = useStore();
         const jwt_token=localStorage.getItem("jwt_token");//取出token
 
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "post",   //  大小写都行
+        //     data: {
+        //         title: "Bot的标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的代码",
+        //     },
+        //     headers: {
+        //         // Authorization: "Bearer " + store.state.user.token,   //  报跨域错误
+        //         Authorization:"Bearer "+ jwt_token, 
+        //     },
+        //     success(resp) {
+        //         console.log("addbot_success" + resp);
+        //     },
+        //     error(resp) {
+        //         console.log("addbot_error" + resp);
+        //     }
+
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 3,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + jwt_token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     },
+        // })
+
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "post",   //  大小写都行
+        //     data: {
+        //         bot_id: 4,
+        //         title: "Bot的标题4",
+        //         description: "Bot的描述4",
+        //         content: "Bot的代码4",
+        //     },
+        //     headers: {
+        //         // Authorization: "Bearer " + store.state.user.token,   //  报跨域错误
+        //         Authorization:"Bearer "+ jwt_token, 
+        //     },
+        //     success(resp) {
+        //         console.log("addbot_success" + resp);
+        //     },
+        //     error(resp) {
+        //         console.log("addbot_error" + resp);
+        //     }
+
+        // })
+
         $.ajax({
-            url: "http://127.0.0.1:3000/user/bot/add/",
-            type: "post",   //  大小写都行
-            data: {
-                title: "Bot的标题",
-                description: "Bot的描述",
-                content: "Bot的代码",
-            },
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
             headers: {
-                // Authorization: "Bearer " + store.state.user.token,   //  报跨域错误
-                Authorization:"Bearer "+ jwt_token, 
+                Authorization: "Bearer " + jwt_token,
             },
             success(resp) {
-                console.log("addbot_success" + resp);
+                console.log(resp);
             },
             error(resp) {
-                console.log("addbot_error" + resp);
-            }
-
+                console.log(resp);
+            },
         })
+
     }
 }
 </script>
