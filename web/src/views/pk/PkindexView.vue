0dc277adde9bf01ac19d4f6bed1cca0b4c193c65
jing-s <jlx1999s@gmail.com>
2024-03-08 15:32:20 +0800

    debug

--- web/src/views/pk/PkindexView.vue
+++ web/src/views/pk/PkindexView.vue
@@ -4,10 +4,39 @@
 
 <script>
 import PlayGround from '../../components/PlayGround.vue'
+// import { onMounted, onUnmounted } from 'vue'
+// import { useStore } from 'vuex'
 
 export default {
     components: {
         PlayGround
+    },
+    setup() {
+        // const store = useStore();
+        // // const socketUrl = `ws:http://127.0.0.1:3000/websocket/${store.state.user.id}/`;
+
+        // let socket = null;
+        // onMounted(() => {
+        //     // socket = new WebSocket(socketUrl);
+
+        //     socket.onopen = () => {
+        //         console.log("connected!");
+        //     }
+
+        //     socket.onmessage = msg => {
+        //         const data = JSON.parse(msg.data);
+        //         console.log(data);
+        //     }
+
+        //     socket.onclose = () => {
+        //         console.log("disconnected!");
+        //     }
+ 
+        // });
+
+        // onUnmounted(() => {
+        //     socket.close();
+        // })
     }
 }
 </script>
